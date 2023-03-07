aaaf16bf618d9d2701fb48c1d9bf376c50d362ca
ImashNipun <it21034404@my.sliit.lk>
2023-03-07 10:56:48 +0530

    update server.js file create server function

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const http = require("http");
 
 
 const server = http.createServer((req,res)=>{
-    res.writeHead(200,"200 success status");
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    res.write("hello world");
     res.end();
 })
 
