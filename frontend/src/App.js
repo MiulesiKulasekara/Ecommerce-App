0ad78ca16b8b60674d4889d85dd206bbe9851b3a
MiulesiKulasekara <it21028878@my.sliit.lk>
2023-03-07 12:10:00 +0530

    Commit pahanya

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import './Components/Miulesi'
 import Miulesi from './Components/Miulesi';
+import Pahanya from './Components/Pahanya';
 
 function App() {
   return (
     <div className="App">
       <Miulesi />
+      <Pahanya />
     </div>
   );
 }
---
+++ frontend/src/Components/Pahanya.js
@@ -0,0 +1,9 @@
+function Pahanya() {
+  return (
+    <>
+      <h3>Hi I am pahanya</h3>
+    </>
+  );
+}
+
+export default Pahanya;
